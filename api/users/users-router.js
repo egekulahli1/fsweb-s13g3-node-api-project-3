78201b8b2174fabccbb3cbab9e04576dc6db83ed
egekulahli <egekulahli@gmail.com>
2023-06-21 05:02:28 +0300

    initial changes

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,44 +1,83 @@
 const express = require('express');
-
+const mw = require('../middleware/middleware.js');
+const userModel = require('./users-model.js');
+const postModel = require('../posts/posts-model.js');
 // `users-model.js` ve `posts-model.js` sayfalarına ihtiyacınız var
 // ara yazılım fonksiyonları da gereklidir
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res, next) => {
   // TÜM KULLANICILARI İÇEREN DİZİYİ DÖNDÜRÜN
+  try {
+      const allUsers = await userModel.get();
+      res.json(allUsers);
+  }
+  catch (error) {
+    next(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // USER NESNESİNİ DÖNDÜRÜN
-  // user id yi getirmek için bir ara yazılım gereklidir
+router.get('/:id',mw.validateUserId, async (req, res, next) => {
+  try {
+    res.json(req.currentUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/', (req, res) => {
-  // YENİ OLUŞTURULAN USER NESNESİNİ DÖNDÜRÜN
-  // istek gövdesini doğrulamak için ara yazılım gereklidir.
+router.post('/', mw.validateUser, async (req, res, next) => {
+    try {
+      const insertedUser = await userModel.insert({ name: req.body.name });
+      res.status(201).json(insertedUser);
+    } catch (error) {
+      next(error);
+    }
 });
 
-router.put('/:id', (req, res) => {
-  // YENİ GÜNCELLENEN USER NESNESİNİ DÖNDÜRÜN
-  // user id yi doğrulayan ara yazılım gereklidir
-  // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
+router.put('/:id', mw.validateUserId, mw.validateUser, async (req, res, next) => {
+  try {
+    const updatedUser = await userModel.update(req.params.id, { name: req.body.name });
+    res.json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', mw.validateUserId, async (req, res, next) => {
   // SON SİLİNEN USER NESNESİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
+  try {
+    await userModel.remove(req.params.id);
+    res.json(req.currentUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', mw.validateUserId, async (req, res, next) => {
   // USER POSTLARINI İÇEREN BİR DİZİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
+  try {
+    const allUserPosts = await userModel.getUserPosts(req.params.id);
+    res.json(allUserPosts);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', mw.validateUserId, mw.validatePost, async (req, res, next) => {
   // YENİ OLUŞTURULAN KULLANICI NESNESİNİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
   // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
+  try {
+    const insertedPost = await postModel.insert({text:req.body.text,user_id:req.params.id});
+    res.json(insertedPost);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // routerı dışa aktarmayı unutmayın
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // require your server and launch it
+const server = require('./api/server.js');
+const port = 9000;
+server.listen(port, () => console.log(`server is listening on ${port}`));
